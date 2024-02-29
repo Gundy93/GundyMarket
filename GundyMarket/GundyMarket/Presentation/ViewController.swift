9f6b29b32598b8ec33f69a616fa9656ec687ba89
Gundy <>
2024-02-29 14:01:58 +0900

    feat: 프로젝트 파일 생성

---
+++ GundyMarket/GundyMarket/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GundyMarket
+//
+//  Created by Gundy on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GundyMarket/GundyMarket/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  GundyMarket
+//
+//  Created by Gundy on 2/29/24.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+    }
+}
